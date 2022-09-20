8a69b896ed2d33e2e32b605879489eb4102a3530
VeraKulemina <verakulemina@gmail.com>
2022-09-19 20:36:40 -0600

    commit tree ca2193acc56cbdddf6b63361def34834e543cea5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog!");
+
+const mondayWork = function(activity1 = "go to the office") {
+    return `This Monday, I will ${activity1}.`;
+}
+
+// function mondayWork(activity1 = "go to the office") {
+//     return `This Monday, I will ${activity1}.`;
+
+// }
+
+function wrapAdjective(flair = "*") {
+    let inner = function(par = "special") {
+        return `You are ${flair}${par}${flair}!`;
+    } 
+    return inner;
+    
+} wrapAdjective("%")("a dedicated programmer");
+//"You are %a dedicated programmer%!"
